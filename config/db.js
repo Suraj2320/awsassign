74c7fe58c7337a16ba431d311f384bda6fb0d8db
Suraj pawar <101104316+Suraj2320@users.noreply.github.co>
2023-03-31 19:47:00 +0530

    k

--- config/db.js
+++ config/db.js
@@ -18,7 +18,9 @@ const mongoose = require("mongoose");
 const dotenv = require('dotenv').config()
 const MONGO = process.env.MONGODB_URL
 mongoose.set('strictQuery', false)
-const connection = mongoose.connect(`${MONGO}`)
+const connection = mongoose.connect(`${MONGO}`,
+{ useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false })
+
 .then(() => console.log("Connected to DataBase"))
   .catch((err) => console.log(err.message));
 
