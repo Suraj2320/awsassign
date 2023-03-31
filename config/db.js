6d7eee579b510d75b3b7c78c3e8c7ce3055d5534
Suraj pawar <101104316+Suraj2320@users.noreply.github.co>
2023-03-31 19:27:48 +0530

    Done

--- config/db.js
+++ config/db.js
@@ -1,15 +1,27 @@
+// const mongoose = require("mongoose");
+// // require("dotenv").config()
+
+// mongoose.set("strictQuery", false);
+
+// const connect = mongoose
+
+//   .connect(
+//     process.env.MONGODB_URL.toString()
+//     // done
+//   )
+//   .then(() => console.log("Connected to DataBase"))
+//   .catch((err) => console.log(err.message));
+
+// module.exports = connect;
+
 const mongoose = require("mongoose");
-// require("dotenv").config()
-
-mongoose.set("strictQuery", false);
-
-const connect = mongoose
-
-  .connect(
-    process.env.MONGODB_URL.toString()
-    // done
-  )
-  .then(() => console.log("Connected to DataBase"))
+const dotenv = require('dotenv').config()
+const MONGO = process.env.MONGODB_URL
+mongoose.set('strictQuery', false)
+const connection = mongoose.connect(`${MONGO}`)
+.then(() => console.log("Connected to DataBase"))
   .catch((err) => console.log(err.message));
 
-module.exports = connect;
+module.exports = {
+  connection
+};
